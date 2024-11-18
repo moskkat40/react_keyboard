8b1151efd17ceb7c64309b0ef10b51cc197a4adb
Kateryna <moskkat40@gmail.com>
2024-11-18 12:31:48 +0100

    add keyboard task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+type State = {
+  pressedKey: boolean;
+  key: string;
+};
+
+export class App extends React.Component<State> {
+  state = {
+    pressedKey: true,
+    key: '',
+  };
+
+  handleKeyboardEvent = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: false, key: event.key });
+  };
+
+  componentDidMount(): void {
+    document.addEventListener('keyup', this.handleKeyboardEvent);
+  }
+
+  componentWillUnmount(): void {
+    document.removeEventListener('keyup', this.handleKeyboardEvent);
+  }
+
+  render() {
+    const { pressedKey, key } = this.state;
+
+    return (
+      <div className="App">
+        {pressedKey ? (
+          <p className="App__message">Nothing was pressed yet</p>
+        ) : (
+          <p className="App__message">The last pressed key is [{key}]</p>
+        )}
+      </div>
+    );
+  }
+}
